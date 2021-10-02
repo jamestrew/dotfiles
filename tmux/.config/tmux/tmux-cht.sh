25784971eb5836363d2fe4514329b781c1b7012b
James Trew <j.trew10@gmail.com>
2021-10-02 19:41:41 -0400

    tmux

---
+++ tmux/.config/tmux/tmux-cht.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+langs=~/.config/tmux/.chtsh-langs
+progs=~/.config/tmux/.chtsh-progs
+selected=`cat "$langs" "$progs" | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ./.chtsh-langs; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "echo \curl cht.sh/$selected~$query | less"
+fi
