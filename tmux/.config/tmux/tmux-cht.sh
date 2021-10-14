59955f2f167e3483bdb2355d9a699b5c04bbbb59
James Trew <j.trew10@gmail.com>
2021-10-13 21:36:38 -0400

    stuff

--- tmux/.config/tmux/tmux-cht.sh
+++ tmux/.config/tmux/tmux-cht.sh
@@ -10,8 +10,8 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" $langs; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    # tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
