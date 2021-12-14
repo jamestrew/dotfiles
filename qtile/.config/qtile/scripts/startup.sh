2ddb1e6413a50dd4e6930da94ccc247fe2f4461e
James Trew <j.trew10@gmail.com>
2021-12-13 22:55:43 -0500

    stuff

--- qtile/.config/qtile/scripts/startup.sh
+++ qtile/.config/qtile/scripts/startup.sh
@@ -2,7 +2,6 @@
 picom &
 imwheel -b 45
 clipmenud &
-xrandr --output DP-1 --primary --mode 3440x1440 --pos 0x480 --rotate normal --rate 144 --output DP-2 --mode 1920x1080 --pos 3440x0 --rotate left --rate 140 --output DP-3 --off --output HDMI-1 --off
 feh --randomize --bg-fill ~/Pictures/wallpapers/*  -- feh set random wallpaper
 teams &
 discord &
