c1a843039a5bfbc23ce50e2c51a6db12c39a1765
James Trew <j.trew10@gmail.com>
2022-07-24 12:49:24 -0400

    changes

--- qtile/.config/qtile/scripts/startup.sh
+++ qtile/.config/qtile/scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 3440x1440 --pos 0x1080 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 760x0 --rotate normal
-picom &
+picom --experimental-backends &
 clipmenud &
-# feh --randomize --bg-fill ~/Pictures/wallpapers/*
+nitrogen --restore &
 discord &
 firefox &
